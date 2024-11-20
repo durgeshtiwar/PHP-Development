616f166370cd2173c87026f3c617a927281abe11
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-11-20 20:56:44 +0530

    Traits Overriding

--- OOPs/Traits.php
+++ OOPs/Traits.php
@@ -1,4 +1,25 @@
 <?php
+// trait ParentCompany_1{
+//   function name_1()
+//   {
+//     echo "This is First Company";
+//   }
+// }
+// trait ParentCompany_2{
+//   function name_2()
+//   {
+//     echo"This is Second Company";
+//   }
+// }
+// class child{
+//   use ParentCompany_1;
+//   use ParentCompany_2;
+// }
+// $obj = new child();
+// $obj->name_1();
+// echo"<br/>";
+// $obj->name_2();
+
 trait ParentCompany_1{
   function name_1()
   {
@@ -6,16 +27,19 @@ trait ParentCompany_1{
   }
 }
 trait ParentCompany_2{
-  function name_2()
+  function name_1()
   {
     echo"This is Second Company";
   }
 }
 class child{
   use ParentCompany_1;
-  use ParentCompany_2;
+  use ParentCompany_2{
+    ParentCompany_1 :: name_1 insteadOf ParentCompany_2;
+    ParentCompany_2 :: name_1 as getName;
+  }
 }
 $obj = new child();
 $obj->name_1();
 echo"<br/>";
-$obj->name_2();
+$obj->getName();
