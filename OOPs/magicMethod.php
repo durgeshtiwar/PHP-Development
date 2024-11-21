0f4d8f0b0d1358895c03d8760d7c5f5d08a9a6db
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-11-21 12:15:46 +0530

    Magic method

---
+++ OOPs/magicMethod.php
@@ -0,0 +1,50 @@
+<?php
+//class User_1{
+  // function function_1()
+  // {
+  //   echo"My Name is Durgesh Tiwari";
+  // }
+//   function __invoke()
+//   {
+//     echo"My Name is Durgesh Tiwari";
+//   }
+// }
+// $obj_1 = new User_1();
+// $obj_1();
+//$obj_1->function_1();
+
+// class User_1{
+//   public $name = "Durgesh Tiwari";
+//   private $name2 = "Shubham Tiwari";
+//   function __get($property)
+//   {
+//     echo"This $property is not exist.";
+//   }
+// }
+// $obj_1 = new User_1();
+// echo $obj_1->namee;
+// echo"<br/>";
+// echo $obj_1->name2;
+
+
+// class User_1{
+//   private $name = "Durgesh";
+//   function __set($property, $value)
+//   {
+//     if (property_exists($this, $property)) {
+//       $this->$property = $value;
+//     }else{
+//     echo"This $property is not exist with $value";
+//     }
+//   }
+//   function function_1()
+//   {
+//     echo"My name is ".$this->name;
+//   }
+// }
+// $obj_1 = new User_1();
+// $obj_1->name = "Durgesh Tiwari";
+// echo"<br/>";
+// echo $obj_1->function_1();
+
+
