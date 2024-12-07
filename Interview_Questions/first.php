fceb5c59a51cfea87b9697dd6989a51b28c4569b
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-12-07 19:28:56 +0530

    Questions

--- Interview_Questions/first.php
+++ Interview_Questions/first.php
@@ -1,6 +1,7 @@
 <?php
-$var_1 = NULL;
-$Var_2 = null;
-var_dump ($var_1 == $Var_2);
-var_dump ($var_1 === $Var_2);
+// $var_1 = NULL;
+// $Var_2 = null;
+// var_dump ($var_1 == $Var_2);
+// var_dump ($var_1 === $Var_2);
+var_dump($var_1);
 ?>
