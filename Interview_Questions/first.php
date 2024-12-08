0892cb11f2a47eed82039aa58a6fe95573d0bd5e
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-12-08 13:46:06 +0530

    practice questions

--- Interview_Questions/first.php
+++ Interview_Questions/first.php
@@ -3,5 +3,9 @@
 // $Var_2 = null;
 // var_dump ($var_1 == $Var_2);
 // var_dump ($var_1 === $Var_2);
+//var_dump($var_1);
+
+//$var_1 = true && false;
+$var_1 = true and false;
 var_dump($var_1);
 ?>
