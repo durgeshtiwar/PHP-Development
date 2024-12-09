57d70a56d710699153f593fe39fcf566d82fe94e
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-12-09 19:21:47 +0530

    Interview question

--- Interview_Questions/first.php
+++ Interview_Questions/first.php
@@ -6,6 +6,32 @@
 //var_dump($var_1);
 
 //$var_1 = true && false;
-$var_1 = true and false;
-var_dump($var_1);
+// $var_1 = true and false;
+// var_dump($var_1);
+
+
+// function chechNum($num)
+// {
+//   if ($num<1) {
+//     return false;
+//   }
+//   for ($i=2; $i<$num/2 ; $i++) { 
+//     if ($num%$i == 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// if (chechNum(12)) {
+//   echo "prime no.";
+// } else {
+//   echo "Not a Prime no.";
+// }
+
+function secondLargest()
+{
+  
+}
+$array = [10, 20, 4, 45, 99];
+
 ?>
