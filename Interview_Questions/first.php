f150e727fad7fb102260ca184c53a7b0eee0dee0
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-12-10 21:46:01 +0530

    practice

--- Interview_Questions/first.php
+++ Interview_Questions/first.php
@@ -29,22 +29,22 @@
 // }
 
 // Patterns 
-$row = 4;
-for ($i= 1; $i <= $row ; $i++) { 
-  for ($j=1,$a = 0, $b = 0; $j <=7 ; $j++) { 
-    if ($j>= $i && $j<= 8- $i) {
-      if ($b<4) {
-        $a = $a+1;
-        echo $a; 
-      }else{
-        $a = $a - 1;
-        echo $a;
-      }
-    }else{
-      echo "&nbspj";
-      $b = $b + 1;
-    }
-  }
-  echo "<br>";
-}
-?>
+// $row = 4;
+// for ($i= 1; $i <= $row ; $i++) { 
+//   for ($j=1,$a = 0, $b = 0; $j <=7 ; $j++) { 
+//     if ($j>= $i && $j<= 8- $i) {
+//       if ($b<4) {
+//         $a = $a+1;
+//         echo $a; 
+//       }else{
+//         $a = $a - 1;
+//         echo $a;
+//       }
+//     }else{
+//       echo "&nbspj";
+//       $b = $b + 1;
+//     }
+//   }
+//   echo "<br>";
+// }
+// ?>
