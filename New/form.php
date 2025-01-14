70d3b6fef5abf85125f851b97c27969e267dbc35
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-01-14 15:20:07 +0530

    button decoration

--- New/form.php
+++ New/form.php
@@ -8,7 +8,7 @@ include_once("request.php");
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="form.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"/>
   <title>Task</title>
 </head>
 <body>
@@ -22,14 +22,15 @@ include_once("request.php");
         veritatis dicta quibusdam? Excepturi, incidunt.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus aliquam tempore accusantium corrupti officia eos qui.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Accusamus aliquam tempore accusantium corrupti officia eos qui.
       </p>
       <h3>Connect with us:</h3>
       <div class="icons">
-      <a><i class="fa-brands fa-facebook"></i></a>
-      <a><i class="fa-brands fa-twitter"></i></a>
-      <a><i class="fa-brands fa-instagram"></i></a>
-      <a><i class="fa-brands fa-linkedin"></i></a>
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-twitter"></i>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-linkedin"></i>
       </div>
       <div class="footer">
       <h3>Made with <i class="fa-solid fa-heart"></i> by Learning Page</h3>
@@ -42,7 +43,7 @@ include_once("request.php");
       <input type="email" placeholder="Enter Your email" name="email" required><br><br>
       <input type="text" placeholder="Enter Your phone" name="phone no." required><br><br>
       <textarea name="message" placeholder="Enter messages here....."></textarea><br><br>
-      <button type="submit">SignUP</button>
+      <button type="submit">SignUp</button>
       </form>
     </div>
   </div>
